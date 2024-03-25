70af51373d15742a3b65b246be128d3faf31cfb8
Abioye Caleb <calebidowu1@gmail.com>
2024-03-25 08:36:07 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	fprintf(stderr,
-			"and thet piece of art is useful" -Dora Korpar, 2015-10-19");
+			"and thet piece of art is useful\" -Dora Korpar, 2015-10-19\n");
 	return (1);
 }
