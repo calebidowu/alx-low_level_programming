6b31cf3cbee5e8bd33105690ee62cb4b4636399b
Abioye Caleb <calebidowu1@gmail.com>
2024-03-25 08:05:52 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Using printf function to output a stderr
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr,
+			"and thet piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return (1);
+}
